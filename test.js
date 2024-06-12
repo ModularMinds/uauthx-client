33c9cf0e30e0329bbe45a2e84634a90cc91d3576
Tanmay Vaij <tanmayvaij22@gmail.com>
2024-06-12 12:06:34 +0530

    removed test file
    Signed-off-by: Tanmay Vaij <tanmayvaij22@gmail.com>

--- test.js
+++
@@ -1,7 +0,0 @@
-const uxc = require("./dist")
-
-const client = new uxc.UAuthXClient("http://localhost:5000")
-
-client.signIn("tanmayvaij22@gmail.com", "tejomay123").then((res) => {
-    console.log(res);
-})
